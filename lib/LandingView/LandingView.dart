6c211ede4f3c40253a96dde8009d7f5e30695715
alexz <prods.alex@gmail.com>
2019-11-17 03:50:03 +0200

    pls work

--- lib/LandingView/LandingView.dart
+++ lib/LandingView/LandingView.dart
@@ -1,54 +1,91 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../Global.dart';
+import '../LoadingView.dart';
 
 var global = Global();
-class LandingView extends StatelessWidget {
 
+class LandingView extends StatefulWidget {
+  @override
+  _LandingViewState createState() => _LandingViewState();
+}
+
+class _LandingViewState extends State<LandingView> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height / 3 * 2,
-            child: Icon(Icons.account_circle, size: MediaQuery.of(context).size.width - 20, color: Colors.blue,),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 15),
-                child: Text("You are not signed in", style: TextStyle(fontFamily: 'Raleway', color: Colors.black.withOpacity(.8), fontSize: 20),),
+    return global.careTaker == null ? Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 60),
+              child: Text(
+                "Sing in",
+                style: TextStyle(
+                    fontFamily: 'Raleway', color: Colors.black87, fontSize: 35, fontWeight: FontWeight.w800),
               ),
-              RaisedButton(
-                child: Text("Sign in", style: TextStyle(fontFamily: 'Raleway', color: Colors.blue),),
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  side: BorderSide(color: Colors.blue)
-                ),
-                onPressed: () {
-                  //FIREBASE AUTH
-                  global.userID = Future.delayed(Duration(seconds: 1));
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 35 , left: 40, right: 40),
+              child: Text("Keep track of your daily activities and never miss out on the important things in life.",
+                maxLines: 3,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Raleway',
+                ),),
+            ),
 
-                },
+            RaisedButton(
+              padding: const EdgeInsets.only(left: 50, right: 50),
+              splashColor: Colors.deepPurple,
+              elevation: 5,
+              child: Text(
+                "I am a pacient",
+                style: TextStyle(fontFamily: 'Raleway', color: Colors.deepPurple,fontSize: 17),
               ),
-              RaisedButton(
-                child: Text("Register", style: TextStyle(fontFamily: 'Raleway', color: Colors.blue),),
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    side: BorderSide(color: Colors.blue)
-                ),
-                onPressed: () {},
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  side: BorderSide(color: Colors.deepPurple)),
+              onPressed: () {
+                setState(() {
+                  global.careTaker = false;
+                  setCareTaker(false);
+                });
+              },
+            ),
+            RaisedButton(
+              padding: const EdgeInsets.only(left: 50, right: 50),
+              splashColor: Colors.deepPurple,
+              elevation: 5,
+              child: Text(
+                "I am a caretaker",
+                style: TextStyle(fontFamily: 'Raleway', color: Colors.deepPurple, fontSize: 17),
               ),
-            ],
-          )
-        ],
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  side: BorderSide(color: Colors.deepPurple)),
+              onPressed: () {
+                setState(() {
+                  global.careTaker = true;
+                  setCareTaker(true);
+                });
+              },
+            ),
+          ],
+        ),
       ),
-    );
+    ) : LoadingView();
+  }
+
+  setCareTaker(bool data) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("careTaker", data);
   }
 }
--- lib/TimelineView/MemoryCard.dart
+++ lib/TimelineView/MemoryCard.dart
@@ -1,12 +1,15 @@
 import 'package:binary_project/DataObjects/EventObject.dart';
+import 'package:binary_project/TimelineView/ExpandedMemory.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MemoryCard extends StatelessWidget {
-  EventObject event;
+  final EventObject event;
+  final double cardWidth = 250;
+
+
   MemoryCard(this.event);
-   final double cardWidth = 250;
 
   @override
   Widget build(BuildContext context) {
@@ -19,38 +22,46 @@ class MemoryCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(15.0),
         ),
         color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.only(
-              bottom: 5.0,
-//              left: 5.0,
-//              right: 5.0
-          ),
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                child: CachedNetworkImage(
-                  imageUrl: event.images.split(",")[0],
-                  placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 8.0,
-                  left: 8.0,
-                  right: 8.0,
-                ),
-                child: new Text(
-                  event.title,
-                  style: new TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Raleway',
-                    fontSize: 20,
+        child: new InkWell(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ExpandedMemory(this.event)),
+            );
+        },
+          child: Padding(
+            padding: const EdgeInsets.only(
+                bottom: 5.0,
+            ),
+            child: new Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  child: CachedNetworkImage(
+                    imageUrl: event.images.split(",")[0],
+                    placeholder: (context, url) => Center(child: CircularProgressIndicator()),
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 8.0,
+                    left: 8.0,
+                    right: 8.0,
+                  ),
+                  child: new Text(
+                    event.title,
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Raleway',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
         elevation: 10,
